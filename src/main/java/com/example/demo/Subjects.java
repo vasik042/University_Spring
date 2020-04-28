b62e69b7ba8ba05c55dbf630c12d531942a3511a
Vasil <vacik2000@ukr.net>
2020-04-28 21:11:42 +0300

    Add subjects enum

---
+++ src/main/java/com/example/demo/Subjects.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+public enum Subjects {
+    UKRAINIAN,
+    MATH,
+    HISTORY,
+    BIOLOGY,
+    GEOGRAPHY,
+    PHYSICS,
+    CHEMISTRY,
+    ENGLISH,
+    SPANISH,
+    GERMAN,
+    FRENCH
+
+
+}
